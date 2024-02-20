7ee4a2eb190b3ac87747ab17695f23d90467b7ac
Jason <jasonetienne1@gmail.com>
2024-02-20 20:02:19 +0000

    removed sensitive file

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://jetienne:ERXE2FEzo99oX5Pt@mern-list-cluster.bmtcb6y.mongodb.net/?retryWrites=true&w=majority'
-}
-
-
-// MongoDB MERN-List-Cluster
-// user: jetienne
-// PW: ERXE2FEzo99oX5Pt
