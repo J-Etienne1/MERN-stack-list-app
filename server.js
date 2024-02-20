8d0b6a62690e4722df7409bde2151dbf565ca30c
Jason <jasonetienne1@gmail.com>
2024-02-20 19:50:40 +0000

    setting up APIs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
-
+const items = require("./routes/api/items")
 
 const app = express();
 
@@ -17,6 +17,11 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
+
+
 
 const port = process.env.PORT || 5000;
 
