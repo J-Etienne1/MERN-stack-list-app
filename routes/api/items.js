7d79e05fba205dd6c229bd081bef00162473973e
Jason <jasonetienne1@gmail.com>
2024-02-21 18:16:27 +0000

    added POST

--- routes/api/items.js
+++ routes/api/items.js
@@ -9,13 +9,23 @@ const Item = require('../../models/item')
 // @route GET api/items
 // @desc Get All Items
 // @access Public
-
-
-
 router.get('/', (req,res) => {
     Item.find()
     .sort({ date : -1 })
     .then(items => res.json(items))
+})
+
+
+
+// @route POST api/items
+// @desc Create A Post 
+// @access Public
+router.post('/', (req,res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
 })
 
 module.exports = router;
