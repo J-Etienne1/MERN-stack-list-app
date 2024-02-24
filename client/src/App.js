2ceb900f7f87cb8ad7a98cd1fe2194fddf85f824
Jason <jasonetienne1@gmail.com>
2024-02-24 12:38:23 +0000

    created Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
+      <AppNavbar/>
     </div>
   );
 }
