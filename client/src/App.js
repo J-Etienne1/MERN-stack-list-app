64dceb62daf1e1b088b22b80bb1ec10cc7f82e47
Jason <jasonetienne1@gmail.com>
2024-02-26 19:40:30 +0000

    wrapped up List component and adding some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import { Button, ListGroup, ListGroupItem } from 'reactstrap'; 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
       <AppNavbar/>
+      
+      <ShoppingList/>
     </div>
   );
 }
 
 export default App;
+
