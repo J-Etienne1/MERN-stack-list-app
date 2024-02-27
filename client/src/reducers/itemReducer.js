aa59cd715eb9cefb2c4c9e4518513efdd7ec32bb
Jason <jasonetienne1@gmail.com>
2024-02-27 19:57:52 +0000

    fixed export

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -15,13 +15,20 @@ const initialState = {
   ],
 };
 
-export default function (state = initialState, action) {
+export default function (state = initialState, action, itemReducer) {
   switch (action.type) {
     case GET_ITEMS:
       return {
         ...state,
       };
+      case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
     default:
       return state;
   }
 }
+
+
